6533ab87c9e3485525a4c23a0ce730fe51c8e0f6
Mark Christiansen <hi@powellmark.com>
2023-05-29 10:55:00 +1000

    Set up note section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  markdown: {
+    drafts: true,
+  },
+  integrations: [
+    mdx({
+      drafts: true,
+    }),
+  ],
   experimental: {
-    assets: true
-  }
+    assets: true,
+  },
 });
