c7820979da3e01211d5783a13ac25154c34b6f24
Mark Christiansen <hi@powellmark.com>
2023-06-01 21:03:04 +1000

    Add photo to about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    drafts: true,
+    drafts: true
   },
-  integrations: [
-    mdx({
-      drafts: true,
-    }),
-  ],
+  integrations: [mdx({
+    drafts: true
+  }), image()],
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
